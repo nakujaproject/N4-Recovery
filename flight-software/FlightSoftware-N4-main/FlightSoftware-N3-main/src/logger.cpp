f9f9a095a25ecb464b0e655a06ad382d2c79017b
bytecod3 <emwiti658@gmail.com>
2024-06-08 15:46:03 +0300

    adds flight computer logger with SPI flash memory

---
+++ flight-software/FlightSoftware-N4-main/FlightSoftware-N3-main/src/logger.cpp
@@ -0,0 +1,129 @@
+/**
+ * Implement onboard logger class functions
+*/
+
+#include "logger.h"
+
+
+/**
+ * 
+ * @brief class constructor 
+ * pass the chip select pin as a parameter for that class instance
+ * 
+ * @param cs_pin chip select pin
+ * @param flas_led LED to show formatting status
+ * @param  filename the filename of the file being created
+ * @param   file_size the size of the file being created
+*/
+Logger::Logger(uint8_t cs_pin, uint8_t flash_led, char filename, uint32_t filesize) {
+    this->_cs_pin = cs_pin;
+    this->_flash_led = flash_led;
+    this->_filename = filename;
+    this->_file_size = filesize;
+}
+
+/**
+ * @brief format the flash memory
+ * @param none
+*/
+void Logger::loggerFormat() {
+    uint8_t id[5];
+    SerialFlash.readID(id);
+    SerialFash.eraseAll();
+
+    // while the flash is formatting, blink the LED at a frequency of 10Hz
+    while(!SerialFlash.ready()) {
+        digitalWrite(this->_flash_led, HIGH);
+        delay(_flash_delay);
+        digitalWrite(this->_flash_led, LOW);
+        delay(_flash_delay);
+    }
+
+    // remain solid lit once formatting is done 
+    digitalWrite(this->_flash_led, HIGH);
+
+}
+
+/**
+ * @brief Initialize the flash memory 
+ * @return true on success and false on fail
+ * 
+*/
+bool Logger::loggerInit() {
+
+
+    if (!SerialFlash.begin(this->_cs_pin)) {
+        return false;
+    } else {
+        // format the flash memory
+        this->loggerFormat();
+
+        // init flash LED
+        pinMode(this->_flash_led, OUTPUT);
+
+        // create logging file with the provided filename
+        if (!SerialFlash.createErasable(this->_filename, this->_file_size)) {
+            return false;
+        }
+
+        return true;
+    }
+}
+
+/**
+ * @brief test the flash memory write and read function by reading and 
+ * writing a variable to it
+ * @param none
+ * @return true if R/W OK, false otherwise
+ * 
+*/
+bool Logger::loggerTest() {
+    // create a string variable 
+    char tst_var[15] = "FlashTesting"
+
+    // write 
+
+    // read 
+
+}
+
+/**
+ * @brief write the provided data to the file created
+ * @param data this is a struct pointer to the struct that contains the data that needs to 
+ * be written to the memory
+ * 
+ * in this function, we write the data structs to the file as comma separated values
+ * 
+*/
+void Logger::loggerWrite(struct* data){
+    // at this point, the flash memory is ready for writing and reading 
+    // check that the passed struct is not null
+    if(data == NULL) {
+        // do sth here 
+        // maybe log error
+    } else {
+        // data valid, ready to proceed
+
+    }
+
+    // TODO: check splitting the passed struct into single values 
+
+    // TODO: maybe return the size of memory written 
+
+    
+
+
+}
+
+/**
+ * @brief Read data from the start of the file to the end of the file 
+ * 
+ * @param _file_pointer pointer to where we want to start reading the file. By default, this value os 0
+ * @param buffer char array to read the data into
+*/
+void Logger::loggerRead(uint8_t file_pointer, char buffer) {
+    // confirm the file exists
+    // seek the file to the start 
+
+
+}
---
+++ flight-software/FlightSoftware-N4-main/FlightSoftware-N3-main/src/logger.h
@@ -0,0 +1,40 @@
+/**
+ * Author: Edwin Mwiti
+ * Email: emwiti658@gmail.com
+ * Date: 7/6/2024
+ * 
+ * This file implements the onboard logging functions for the flight computer 
+ * We use the SerialFlash library by Paul, link to the library is provided in the references
+ * section
+ * 
+ * If you are using more than one SPI flash memory, create an instance for each SPI flash memory
+ * 
+*/
+
+#ifndef LOGGER_H
+#define  LOGGER_H
+
+#include <Arduino.h>
+#include <SerialFlash.h>
+
+class Logger {
+    public:
+        uint8_t _cs_pin;         // Chip select pin for the SPI flash memory
+        uint8_t _flash_led;      // this LED flashes at a 10Hz frequency when the SPI flash chip is foramtting
+        char _filename[15];      // filename of the log file
+        uint32_t _file_size;    // how large do you want the file 
+
+    private:
+        uint8_t _flash_delay = 100; // 100ms delay gives a frequency of 20Hz
+        uint8_t _file_pointer = 0;   // pointer to the start of the file- to be used when reading the file 
+
+        Logger(uint8_t CS_PIN); // constructor
+        bool loggerInit();
+        void loggerFormat();
+        bool loggerTest();
+        void loggerWrite(struct* data);
+        void loggerRead(uint8_t file_pointer, char buffer);
+};
+
+
+#endif
