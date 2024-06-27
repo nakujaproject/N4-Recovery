c128b507736ec7b1c91389f27b04684761a07e52
bytecod3 <emwiti658@gmail.com>
2024-06-27 14:02:43 +0300

    adds working remote switching MQTT

--- remote-switching/MQTT/remote-switching-mqtt/remote-switching-mqtt.ino
+++ remote-switching/MQTT/remote-switching-mqtt/remote-switching-mqtt.ino
@@ -10,12 +10,12 @@
 #include <PubSubClient.h>
 
 // Wifi
-const char* ssid = "";  // your wifi name
-const char* password = ""; // your wifi password
+const char* ssid = "Nakuja";  // your wifi name
+const char* password = "987654321"; // your wifi password
 
 // MQTT broker
-const char* mqtt_broker = "broker.emqx.io";
-const char* topic = "emqx/esp32";
+const char* mqtt_broker = "broker.emqx.io"; // use the IP ADDRESS of the computer running the MQTT broker
+const char* topic = "rocket/arm";
 const char* mqtt_username = "emqx";
 const char* mqtt_password = "public";
 const int mqtt_port = 1883;
@@ -23,7 +23,7 @@ const int mqtt_port = 1883;
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-#define ARMING_PIN 4
+#define ARMING_PIN 13
 
 /** 
  * implements remote switching for the avionics 
@@ -37,6 +37,23 @@ void armAvionics(){
  */
 void checkArming(){
   
+}
+
+/**
+ * Parse command received from MQTT
+ */
+void parseMQTTCommand(String command) {
+  Serial.print("Calling parser with command: ");
+  Serial.println(command);
+  
+  if(command == "\"ARM\"") {
+    Serial.println("Rocket armed");
+    digitalWrite(ARMING_PIN, HIGH);
+  } else if (command == "\"DISARM\"") {
+    Serial.println("Rocket disarmed");
+    digitalWrite(ARMING_PIN, LOW);
+  }
+
 }
 
 void setup() {
@@ -55,11 +72,11 @@ void setup() {
   // connecting to MQTT broker
   client.setServer(mqtt_broker, mqtt_port);
   client.setCallback(callback);
-  while(!client.connected) {
+  while(!client.connected()) {
     String client_id = "esp32_client-";
-    client_id_ += String(WiFi.macAddress());
+    client_id += String(WiFi.macAddress());
 
-    if(client.connect(client_id.c_str(), mqtt_username, mqtt_password) {
+    if(client.connect(client_id.c_str(), mqtt_username, mqtt_password)) {
       Serial.println("Connected to public MQTT broker");    
     } else {
       Serial.print("Failed with state "); 
@@ -69,10 +86,16 @@ void setup() {
   }
 
   // publish and subscribe
-  client.publish(topic, "Hi, I'm ESP32");
+//  client.publish(topic, "Hi, I'm ESP32");
+  client.subscribe(topic);
 
   // set up the avionics arming pin
   pinMode(ARMING_PIN, OUTPUT); 
+
+  String name = "ESP";
+  if(name == "ESP") {
+    Serial.println("Name is ESP");
+  }
 }
 
 void loop() {
@@ -85,11 +108,13 @@ void loop() {
 void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Message arrived on topic: ");
   Serial.println(topic);
-  Serial.print("Message: ");
+  String command;
+  
   for (int i=0; i<length; i++) {
-    Serial.println((char) payload[i]);
+    command += (char) payload[i]; 
   }
 
-  Serial.println();
+  parseMQTTCommand(command);
+  
   Serial.println("---------------------------------------");
 }
